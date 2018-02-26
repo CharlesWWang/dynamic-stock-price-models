05bee4329329f58b649ddebbe06fbac7974a2db0
Sergey Kojoian <kojoiansergey@gmail.com>
2018-02-26 09:10:20 -0800

    simulation runner implemented.

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 """Model initialization script."""
+from models import HeuristicalModel
+from heuristics import *
+
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
@@ -9,6 +12,7 @@ if __name__ == '__main__':
     parser.add_argument(
             'weights',
             nargs='+',
+            type=float,
             help='weights for each excess demand function')
     parser.add_argument(
             'w', 
@@ -23,6 +27,10 @@ if __name__ == '__main__':
             type=int,
             help='number of steps to perform during the initial random walk')
     parser.add_argument(
+            'steps',
+            type=int,
+            help='number of steps to perform after the random walk')
+    parser.add_argument(
             'num_trials',
             type=int,
             help='number of simulations to perform')
@@ -33,3 +41,24 @@ if __name__ == '__main__':
             help='increase output verbosity')
     
     args = parser.parse_args()
+    
+    heuristics = [eval(heuristic) for heuristic in args.heuristics]
+    model = HeuristicalModel(
+            heuristics,
+            args.weights,
+            args.w)
+
+    model.generate_init_data(args.rand_steps, args.init_price)    
+    results = np.zeros((args.num_trials, args.rand_steps + args.steps),
+                       dtype=np.float32)
+    
+    for i in range(args.num_trials):
+        results[i] = model.simulate(args.steps)
+
+    if args.verbose:
+        f, axis = plt.subplots(args.num_trials, sharex=True)
+        f.subplots_adjust(hspace=0.5)
+        for i in range(args.num_trials):
+            axis[i].plot(results[i])
+            axis[i].axvline(args.rand_steps, color='red')
+        plt.show()
